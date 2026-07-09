34ef78f1527db36d35f745ce16bc9aac4f693947
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-07-09 10:51:23 +0300

    add health endpoint

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
     res.end("Welcome to the Book store!");
 });
  
+app.get("/health", (req, res) => {
+    res.json({health : "Server is running"});
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}`));
