d36a8eb361698bafa2bbf383222729c1cb2a99fa
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-07-09 15:05:11 +0300

    Add GET cart by customer ID

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,32 @@
 import express from "express";
 
+import {
+    loadFromJson,
+    saveToJson
+} from "../service/jsonService.js";
+import { error } from "node:console";
+
 const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    const customers = loadFromJson("data/customers.json");
+
+    const {customerId} = req.query;
+
+    if(!customerId){
+        return res.status(400).json({error : "must recieve customerId value"})
+    }
+
+    const customer = customers.find(customer => customer.customerId === customerId);
+
+    if(!customer){
+        return res.status(404).json({error : "Customer not found"});
+    }
+    
+    res.json(customer.cart);
+});
+
+
 
 export default router;
